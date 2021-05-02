7d16a1eb187ef58d7df3e0621779b72f9db90cae
Angel Rojo Marijuan <rojo.angel@gmail.com>
2021-05-02 11:54:22 +0200

    init repo

---
+++ src/main/java/com/orelly/restclientdemo/RestclientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.orelly.restclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestclientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestclientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orelly/restclientdemo/RestclientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orelly.restclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
