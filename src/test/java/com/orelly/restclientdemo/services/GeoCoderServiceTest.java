0137d093f3d8372baa8e031ffe8e971428ec6da1
Angel Rojo Marijuan <rojo.angel@gmail.com>
2021-05-03 11:36:39 +0200

    fix issue with retrieved formatted address

--- src/test/java/com/orelly/restclientdemo/services/GeoCoderServiceTest.java
+++ src/test/java/com/orelly/restclientdemo/services/GeoCoderServiceTest.java
@@ -2,10 +2,13 @@ package com.orelly.restclientdemo.services;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 import com.orelly.restclientdemo.entities.Site;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,12 +18,16 @@ class GeoCoderServiceTest {
   @Autowired
   GeoCoderService service;
 
+  private Logger logger = LoggerFactory.getLogger(GeoCoderServiceTest.class);
+
   @Test
   void getLatLngWithoutStreet() {
     Site site = service.getLatLng("Boston", "MA");
+    logger.info(site.toString());
     assertAll(
         () -> assertEquals(42.36, site.getLatitude(), 0.01),
-        () -> assertEquals(-71.06, site.getLongitude(), 0.01)
+        () -> assertEquals(-71.06, site.getLongitude(), 0.01),
+        () -> assertNotNull(site.getAddress())
     );
   }
 
@@ -28,9 +35,11 @@ class GeoCoderServiceTest {
   void getLatLngWithStreet() {
     Site site = service.getLatLng("1600 Ampitheatre Parkway",
         "Mountain View", "CA");
+    logger.info(site.toString());
     assertAll(
         () -> assertEquals(37.43, site.getLatitude(), 0.01),
-        () -> assertEquals(-122.08, site.getLongitude(), 0.01)
+        () -> assertEquals(-122.08, site.getLongitude(), 0.01),
+        () -> assertNotNull(site.getAddress())
     );
   }
 }
